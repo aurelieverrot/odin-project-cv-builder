2aa73dafb03aea848f90823b053fc59d49c71bea
Aurelie Verrot <aurelie.verrot@gmail.com>
2021-03-27 16:23:48 -0700

    Add Experience block

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import GeneralInformation from "./components/GeneralInformation";
+import Experience from "./components/Experience";
+import React from "react";
 
 function App() {
 
@@ -7,9 +9,16 @@ function App() {
   return (
     <div className="App">
         <h1>Create Your Resume</h1>
-        < GeneralInformation/>
+        <div className='container-md'>
+            <h2>General Information</h2>
+            < GeneralInformation/>
+        </div>
+        <div className='container-md'>
+            <h2>Professional Experience</h2>
+            < Experience/>
+        </div>
     </div>
   );
-}
+};
 
 export default App;
