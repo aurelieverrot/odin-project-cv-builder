033cb5b59a4708fc58220b7f4af830e2ea557cfa
Aurelie Verrot <aurelie.verrot@gmail.com>
2021-03-27 15:35:48 -0700

    Add CSS rules with Bootstrap

--- src/components/GeneralInformation.js
+++ src/components/GeneralInformation.js
@@ -31,88 +31,105 @@ function GeneralInformation() {
         return (
             <div>
                 <form className="general-info" onSubmit={onSubmit}>
-                    <div>
-                        <label>Full Name</label>
-                        <input
+                    <div className="row mb-3">
+                        <label className="col-sm-3 col-form-label">Full Name</label>
+                        <div className="col-sm-9">
+                            <input
+                            className="form-control"
                             type="text"
                             name="fullName"
                             placeholder="First Last"
                             value={state.fullName}
                             onChange={handleChange}/>
+                        </div>
                     </div>
 
-                    <div>
-                        <label>Email</label>
-                        <input
-                            type="text"
-                            name="email"
-                            placeholder="your@email.com"
-                            value={state.email}
-                            onChange={handleChange}/>
+                    <div className="row mb-3">
+                        <label className="col-sm-3 col-form-label">Email</label>
+                        <div className="col-sm-9">
+                            <input
+                                className="form-control"
+                                type="text"
+                                name="email"
+                                placeholder="your@email.com"
+                                value={state.email}
+                                onChange={handleChange}/>
+                        </div>
                     </div>
 
-                    <div>
-                        <label>Phone Number</label>
+                    <div className="row mb-3">
+                        <label className="col-sm-3 col-form-label">Phone Number</label>
+                        <div className="col-sm-9">
                         <input
+                            className="form-control"
                             type="text"
                             name="phone"
                             placeholder="XXX-XXX-XXXX"
                             value={state.phone}
                             onChange={handleChange}/>
+                        </div>
                     </div>
 
-                    <div>
-                        <label>LinkedIn Profile</label>
-                        <input
-                            type="text"
-                            name="linkedIn"
-                            placeholder="linkedin.com/<your-name>"
-                            value={state.linkedIn}
-                            onChange={handleChange}/>
+                    <div className="row mb-3">
+                        <label className="col-sm-3 col-form-label">LinkedIn Profile</label>
+                        <div className="col-sm-9">
+                            <input
+                                className="form-control"
+                                type="text"
+                                name="linkedIn"
+                                placeholder="linkedin.com/<your-name>"
+                                value={state.linkedIn}
+                                onChange={handleChange}/>
+                        </div>
                     </div>
 
-                    <div>
-                        <label>Github Profile</label>
-                        <input
-                            type="text"
-                            name="github"
-                            placeholder="github.com/<your-name>"
-                            value={state.github}
-                            onChange={handleChange}/>
+                    <div className="row mb-3">
+                        <label className="col-sm-3 col-form-label">Github Profile</label>
+                        <div className="col-sm-9">
+                            <input
+                                className="form-control"
+                                type="text"
+                                name="github"
+                                placeholder="github.com/<your-name>"
+                                value={state.github}
+                                onChange={handleChange}/>
+                        </div>
                     </div>
 
-                    <input type="submit" />
+                    <input type="submit" className="btn btn-outline-primary float-end" />
                 </form>
             </div>
         )
     } else {
         return (
             <div>
-                <div>
-                    <label>Full Name</label>
-                    <p>{state.fullName}</p>
-                </div>
+                <form>
+                    <div className="row-mb-3">
+                        <label className="col-sm-3 col-form-label">Full Name</label>
+                        <p className="col-sm-9">{state.fullName}</p>
+                    </div>
 
-                <div>
-                    <label>Email</label>
-                    <p>{state.email}</p>
-                </div>
+                    <div className="row-mb-3">
+                        <label className="col-sm-3 col-form-label">Email</label>
+                        <p className="col-sm-9">{state.email}</p>
+                    </div>
 
-                <div>
-                    <label>Phone Number</label>
-                    <p>{state.phone}</p>
-                </div>
+                    <div className="row-mb-3">
+                        <label className="col-sm-3 col-form-label">Phone Number</label>
+                        <p className="col-sm-9">{state.phone}</p>
+                    </div>
 
-                <div>
-                    <label>LinkedIn Profile</label>
-                    <p>{state.linkedIn}</p>
-                </div>
+                    <div className="row-mb-3">
+                        <label className="col-sm-3 col-form-label">LinkedIn Profile</label>
+                        <p className="col-sm-9">{state.linkedIn}</p>
+                    </div>
 
-                <div>
-                    <label>Github Profile</label>
-                    <p>{state.github}</p>
-                </div>
-                <button onClick={onSubmit}>Edit General Information</button>
+                    <div className="row-mb-3">
+                        <label className="col-sm-3 col-form-label">Github Profile</label>
+                        <p className="col-sm-9">{state.github}</p>
+                    </div>
+                    <button onClick={onSubmit} className="btn btn-outline-secondary float-end">Edit</button>
+                </form>
             </div>
         )
     }
